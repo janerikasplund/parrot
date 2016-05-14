fcdea793da09cf323f3da2e14f2efa39a807d7b4
Jan-Erik Asplund <janerik.asplund@gmail.com>
2016-05-14 15:29:02 -0400

    Cleanup

---
+++ transcribe.py
@@ -0,0 +1,47 @@
+"""This code allows a user to upload a
+file to the server and then have it
+processed into a Google Cloud API-friendly
+FLAC file"""
+
+import glob, time, os, subprocess
+from flask import Flask, request, redirect, url_for, render_template
+from werkzeug.utils import secure_filename
+
+UPLOAD_FOLDER = '/home/jan/parrot/songs/'
+ALLOWED_EXTENSIONS = set(['flac', 'mp3', 'wav'])
+
+app = Flask(__name__)
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1] in ALLOWED_EXTENSIONS
+
+@app.route('/', methods=['GET', 'POST'])
+def upload_file():
+    if request.method == 'POST':
+        file = request.files['file']
+        if file and allowed_file(file.filename):
+            filename = secure_filename(file.filename)
+            stringname, file_extension = os.path.splitext(filename)
+            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+            for file.filename in sorted(glob.glob("/home/jan/parrot/songs/%s" % filename)):
+                subprocess.call('mkdir /home/jan/parrot/songs/%s' % stringname, shell=True)
+                subprocess.call('sox /home/jan/parrot/songs/%s --channels=1 --bits=16 /home/jan/parrot/songs/%s/%s.flac -q trim 0 50 : newfile : restart' % (filename, stringname, stringname), shell=True)
+                os.chdir("/home/jan/parrot/songs/%s" % stringname)
+            for file in sorted(glob.glob(stringname + "0*.flac")):
+                subprocess.call('python /home/jan/parrot/speech_rest.py /home/jan/parrot/songs/%s/%s >>/home/jan/parrot/songs/%s/%s-transcript.txt' % (stringname, file, stringname, stringname), shell=True)
+                time.sleep(10)
+            return render_template('hello2.html')
+    return render_template('hello.html')
+
+
+
+
+# from flask import send_from_directory
+#@app.route('/transcripts/<filename>')
+#def uploaded_file(filename):
+#    return send_from_directory(app.config['UPLOAD_FOLDER'],
+#                               filename)
+
+if __name__ == '__main__':
+    app.run(debug=True)
